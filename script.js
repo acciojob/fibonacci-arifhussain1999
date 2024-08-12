80945b587d1d9d750dc8a0e509eb652a7f965ae2
Arif Hussain <arifh9821@gmail.com>
2024-08-13 01:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if(num<=1) {
+		return num;
+	}
+	
+	let a=0, b=1,temp;
+	for(let i=2;i<=num;i++){
+		temp=a+b;
+		a=b;
+		b=temp;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
